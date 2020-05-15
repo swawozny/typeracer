a0353264bfa707abc9e2fa9720a7a68fff75ce20
tumilok <vladislav.tumilovich@gmail.com>
2020-05-15 13:09:33 +0200

    Changed context_processors

--- typeracer/context_processors.py
+++ typeracer/context_processors.py
@@ -1,5 +1,3 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.db.models import Avg
 
 from typer.models import Game
@@ -7,10 +5,12 @@ from typer.models import Game
 
 def statisitcs(request):
     if not request.user.is_authenticated:
-        return{}
+        return {}
     else:
-        avgwpm = Game.objects.all().filter(user=request.user).aggregate(Avg('wpm'))['wpm__avg']
-        avgcpm = Game.objects.all().filter(user=request.user).aggregate(Avg('cpm'))['cpm__avg']
-        lastgame = Game.objects.all().filter(user=request.user).order_by('-date')[0]
-        return {'wpm': avgwpm, 'cpm': avgcpm, 'lastgame': lastgame}
-
+        if Game.objects.all().filter(user=request.user).exists():
+            avgwpm = Game.objects.all().filter(user=request.user).aggregate(Avg('wpm'))['wpmavg']
+            avgcpm = Game.objects.all().filter(user=request.user).aggregate(Avg('cpm'))['cpmavg']
+            lastgame = Game.objects.all().filter(user=request.user).order_by('-date')[0]
+            return {'wpm': avgwpm, 'cpm': avgcpm, 'lastgame': lastgame}
+        else:
+            return {'wpm': 'BRAK', 'cpm': 'BRAK', 'lastgame': []}
