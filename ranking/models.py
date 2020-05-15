f4441aa71af1215b079817dd4a496ffd42329f32
tumilok <vladislav.tumilovich@gmail.com>
2020-05-15 19:06:00 +0200

    Modified ranking app

--- ranking/models.py
+++ ranking/models.py
@@ -1,10 +1,3 @@
-from django.db import models
 
 
-class Osoba(models.Model):
-    name = models.CharField(max_length=30)
-    body = models.SlugField()
-    date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
